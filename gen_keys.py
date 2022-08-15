14baf3f639ac3c35df05371c5d1a098cf140193f
msla1219 <76463854+msla1219@users.noreply.github.com>
2022-08-15 10:42:08 +0900

    Update gen_keys.py

--- gen_keys.py
+++ gen_keys.py
@@ -5,4 +5,27 @@ from algosdk import account
 from web3 import Web3
 
 
+def get_eth_key(eth_mnemonic):
+    try:
 
+        w3 = Web3()
+        w3.eth.account.enable_unaudited_hdwallet_features()
+        acct = w3.eth.account.from_mnemonic(eth_mnemonic)
+        eth_pk = acct._address
+
+        return eth_pk
+
+    except Exception as e:
+        print("Couldn't get Ethereum server pk: ", eth_pk)
+        print(e)
+
+
+def get_algo_key(algo_mnemonic):
+    try:
+
+        algo_pk = mnemonic.to_public_key(algo_mnemonic)
+
+        return algo_pk
+
+    except Exception as e:
+        print("Couldn't get Ethereum server pk: ", algo_pk)
