6039e9592f85ff027b2e17eb64dd3af1d383e63e
msla1219 <76463854+msla1219@users.noreply.github.com>
2022-08-15 19:20:18 +0900

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -26,29 +26,24 @@ def connect_to_algo(connection_type=''):
 
 def send_tokens_algo(acl, sender_sk, txes):
 
-    params = acl.suggested_params()
-
-    gh = params.gh
-    first_valid_round = params.first
-    last_valid_round = params.last
-    fee = params.min_fee
 
     # TODO: You might want to adjust the first/last valid rounds in the suggested_params
     #       See guide for details
+    sp = acl.suggested_params()
 
-    # params.last = params.first + 800
+    sp.last = sp.first + 800 + 0  # Algorand requires sp.last - sp.first < 1000
+
+    sender_pk = account.address_from_private_key(sender_sk)
 
     # TODO: For each transaction, do the following:
     #       - Create the Payment transaction
     #       - Sign the transaction
 
     # TODO: Return a list of transaction id's
-    sender_pk = acl.address_from_private_key(sender_sk)
-
     tx_ids = []
 
     for i, tx in enumerate(txes):
-        unsigned_tx = transaction.PaymentTxn(sender_sk, params, tx['receiver_pk'], tx['amount'])
+        unsigned_tx = transaction.PaymentTxn(sender_pk, sp, tx['receiver_pk'], tx['amount'])
 
         # TODO: Sign the transaction
         signed_tx = unsigned_tx.sign(sender_sk)
