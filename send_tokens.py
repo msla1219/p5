6ce307a6e42134ab4144f775f2d6310eb96b5efe
msla1219 <76463854+msla1219@users.noreply.github.com>
2022-08-15 15:13:07 +0900

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -56,9 +56,6 @@ def send_tokens_algo(acl, sender_sk, txes):
 
             # TODO: Send the transaction to the testnet
             tx_id = acl.send_transaction(signed_tx)
-
-            time.sleep(5)
-
             txinfo = wait_for_confirmation_algo(acl, txid=tx_id)
             print(f"Sent {tx['amount']} microalgo in transaction: {tx_id}\n")
         except Exception as e:
@@ -76,9 +73,6 @@ def wait_for_confirmation_algo(client, txid):
     """
     last_round = client.status().get('last-round')
     txinfo = client.pending_transaction_info(txid)
-
-    time.sleep(5)
-
     while not (txinfo.get('confirmed-round') and txinfo.get('confirmed-round') > 0):
         print("Waiting for confirmation")
         last_round += 1
