37c119683f42343adcf9faffc1462d6c29ffa6c9
msla1219 <76463854+msla1219@users.noreply.github.com>
2022-08-15 11:12:15 +0900

    Update gen_keys.py

--- gen_keys.py
+++ gen_keys.py
@@ -4,23 +4,25 @@ from algosdk import mnemonic
 from algosdk import account
 from web3 import Web3
 
+# mnemonic to generate the public key for the exchange server
+eth_mnemonic = "midnight game play tail blossom cereal jacket cruel okay slim verify harbor"
+algo_mnemonic = "avocado coil energy gallery health brief crime peanut coyote brother coach bullet december limit oblige answer town bar neck provide ivory cousin custom abstract demise"
 
-def get_eth_key(eth_mnemonic):
+
+def eth():
     try:
 
         w3 = Web3()
         w3.eth.account.enable_unaudited_hdwallet_features()
         acct = w3.eth.account.from_mnemonic(eth_mnemonic)
-        eth_pk = acct._address
-
-        return eth_pk
+        return acct._address
 
     except Exception as e:
-        print("Couldn't get Ethereum server pk: ", eth_pk)
+        print("Couldn't get Ethereum server")
         print(e)
 
 
-def get_algo_key(algo_mnemonic):
+def algo():
     try:
 
         algo_pk = mnemonic.to_public_key(algo_mnemonic)
@@ -28,4 +30,4 @@ def get_algo_key(algo_mnemonic):
         return algo_pk
 
     except Exception as e:
-        print("Couldn't get Ethereum server pk: ", algo_pk)
+        print("Couldn't get Algorand server pk")
